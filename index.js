f149dcfef33102578554546dc5188aef99731435
duke1014 <duke.norsworthy@gmail.com>
2021-04-27 17:17:52 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+// multiply
+var num1 = 31;
+var num2 = 2;
+var multiply = (num1 * num2);
+
+// random
+var random = (Math.floor(Math.random() * 10) + 1);
+
+// mod
+var num3 = 104;
+var num4 = 25;
+var mod = (num3 % num4);
+
+// max
+var max = Math.max(4, 8, 12, 16, 20);
